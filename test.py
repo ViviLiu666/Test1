5a368b0220a96fa5f26a362f16c8de070b52e44e
Vivi <vivi@test.com>
2025-02-15 10:11:20 +0800

    add test py

--- test.py
+++ test.py
@@ -1,3 +1,7 @@
 print("Hello world")
 # 到Terminal打git init, 將資料夾轉為git操控
-# 打git status, 追蹤資料夾狀態
+# 打git status, 追蹤資料夾狀態, 紅字為untrack狀態
+# 打git add . (add空格點),綠字為"詢問"把資料夾檔案變成一個版本
+# 打git commit -m "add test py" 要多人協作
+# Terminal的複製貼上不要用Ctrl+C/V,只需選取要的部分案右鍵,再按右鍵即可貼上
+# 跑完上述流程,建立新版本
