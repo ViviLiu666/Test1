d7ad896774bed2bc54054ecca4f4da547bcb5295
Vivi <vivi@test.com>
2025-02-15 12:01:06 +0800

    add gitignore

--- test.py
+++ test.py
@@ -12,3 +12,10 @@ c = a+b
 # 再source control的輸入欄輸入要置換的版本名稱,然後按commit自動存檔=committed
 print(c)
 # 若要將以修改版本退回未修改版本,指令git restore --staged <file name不須外框>
+# 若要回去之前的版本,指令 git reset --hard(版本編碼) ->只看不改,後面會消失
+# 若不要中間版本消失,不能修改,指令 git reflog ->查看所有版本編碼, 再次下指令 git reset --hard最新版
+# 但若是回到之前版本並修改,原先的版本就強制消失
+a = 3
+b = 1
+# 指定一些檔案永遠不要進到我的資料庫
+# 新增檔案 .gitignore 並將不想要的檔案名稱打進去, 確認狀態git status
